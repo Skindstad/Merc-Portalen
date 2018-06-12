554027fd58e4ab59a29ea5140edbc8104882d611
Skindstad <jako5805@edu.mercantec.dk>
2018-06-12 13:48:42 +0100

    arbejde på Profil og created DBh
    Jeg har lavet en DBh den er ikke helt oppe og køre i nu så der stadig nogle få ting som mangler blivet lavet. Jeg har også lave på Profil den mangler på blive stad til  Databasen.

---
+++ profil.php
@@ -0,0 +1,31 @@
+<?php 
+
+include'navbar.php';
+//include_once 'DBh.php'
+
+?>
+
+<body>
+
+<div>
+<img src="https://www.hooked4pets.dk/images/design/billeder/slider_forsiden_photo11.jpg">
+</div>
+<div>
+<h2>Dit navn</h2>
+<div>
+<br>
+<div>
+<h3>Om dig<h3>
+<p>Alder<P>
+<p>Fødselsdag<p>
+</div>
+<br>
+<div>
+<h3>Dine forums</h3>
+
+</div>
+
+
+
+
+<body>
