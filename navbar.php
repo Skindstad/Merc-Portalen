ac4433168c83c651861ff6c8704499c65da43bcf
Kenneth Jensen <kenneth_jensen_99@hotmail.com>
2018-06-12 11:27:15 +0200

    Edited navbar and style

---
+++ navbar.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+<body>
+
+
+<div class="topbar">
+       </div>
+
+<ul> 
+        <li><a href="index.php">Hjem</a></li>
+        <li><a href="kontakt.php">Kontakt</a></li>
+        <li><a href="Forum.php">Forum</a></li> 
+        <li><a href="gaestebog.php">Gæstebog</a></li> 
+        <li><a href="profil.php">Profil</a></li> 
+        <li><a href="login.php"Login</a></li> 
+        <li><a href="opret.php">Opret konto</a></li> 
+ </ul>
+      
+
+
+</body>
+</html> 
