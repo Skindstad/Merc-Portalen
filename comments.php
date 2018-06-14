011edb707b73fb4fd1d5847cbbfd2803e4dcb50d
Skindstad <jako5805@edu.mercantec.dk>
2018-06-14 13:07:36 +0100

    Comments done

--- comments.php
+++ comments.php
@@ -7,28 +7,11 @@ include_once 'DBh.php';
 $Sender = $_POST['Sender'];
 $Content = $_POST['Content'];
 
-	$sql = "INSERT INTO comment (profilId,  forumId, content) VALUES ('$Sender','?', '$Content');";
+	$sql = "INSERT INTO comment (profilId,  forumId, content) VALUES ('$Sender','3', '$Content');";
 	mysqli_query($conn, $sql);
 
 	header("Location: ../Merc-Portalen/comment.php?=success");
 	
-
-/*funtion setComments($conn) {
-	if (isset($_POST['commentSubmit'])) {
-		$profilId = $_POST['Sender'];
-		$message = $_POST['Content'];
-
-		$sql = "INSERT INTO comment (profilId, forumId, content) VALUES ('$profilId', '?', '$message')";
-		$result = $conn->query($sql)
-	}
-}
-*/
-funtion getComments($conn) {
-	$sql = "SELECT * FROM comment";
-	$result = $conn->query($sql);
-	$row = $result->fetch_assoc();
-	echo $row['message']
-}
 
 
 	?>
