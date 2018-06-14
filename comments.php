23307bf1a9db2d90bbe58837f0e44bfb6f02abf1
Skindstad <jako5805@edu.mercantec.dk>
2018-06-14 09:34:20 +0100

    Små dele som jeg har rettet

--- comments.php
+++ comments.php
@@ -5,9 +5,10 @@ include_once 'DBh.php';
 
 
 $Sender = $_POST['Sender'];
+$forum = $_POST['forum'];
 $Content = $_POST['Content'];
 
-	$sql = "INSERT INTO comment (profilId,  , content) VALUES ('$navn', '$content');";
+	$sql = "INSERT INTO comment (profilId,  forumId, content) VALUES ('$Sender','$forum', '$Content');";
 	mysqli_query($conn, $sql);
 
 	header("Location: ../Merc-Portalen/forum.php?comments=success");
--- forum.php
+++ forum.php
@@ -18,9 +18,12 @@ include 'DBh.php';
 <h1>Nye forums</h1>
 <div>
 	<form class="forum" action="forums.php" method="POST">
-		 <input type="text" name="navn" placeholder="1 or up"><br>
-        <input type="text" name="content" placeholder="Skive hvad du synes.">
+		<div class="form-group">
+		 <input type="text" name="navn" placeholder=" profil: 1 or up"><br>
+		</div><div class="form-group">
+        <textarea class="content" type="text" name="content" placeholder="Skive hvad du synes."></textarea>
         <br>
+    </div>
         <button type="submit" name="submit">Send</button>
 	</form>
 	
@@ -37,16 +40,15 @@ include 'DBh.php';
 	$result2 = mysqli_query($conn, $Mysql);
 	$resultCheck = mysqli_num_rows($result);
 	$resultCheck2 = mysqli_num_rows($result2);
-	if ($resultCheck AND $resultCheck2 > 0) {
-	//if ($resultCheck2 > 0) {
-		while ($row = mysqli_fetch_array($result)) {
-			echo "<div>" . "<h2>" . $row['fuldeNavn'] . "</h2>" . "<br>" . "<h4>" . $row['newTime'] . "</h4>" . "<p>" . $row['content'] . "</p>". "<br>". "</div>" ;
-			while ($row = mysqli_fetch_array($result2)) {
-			echo "<div>" . "<h3>" . 'Comments' . "</h3>" . "<h3>" . $row['fuldeNavn'] . "</h3>" . "<h4>" . $row['newTime'] . "</h4>" . "<p>" . $row['content'] . "</p>". "<br>". "</div>" ;
-}
-			echo  "<br>" . "<div>" . "<form class='comment' action='comments.php' method='POST'>" . "<input type='text' name='Sender' placeholder='1 or up'>". "<br>" . "<input type='text' name='forum' placeholder='1 og op efter.'>" ."<input type='text' name='Content' placeholder='Skive hvad du synes.'>" . "<br>" . "<button type='submit' name='submit'>Send</button>" . "</form>" . "</div>";
 
-	}
+	if ($resultCheck AND $resultCheck2 > 0) {
+		while ($row = mysqli_fetch_array($result)) {
+			echo "<div>" . "<h2>" . $row['fuldeNavn'] . "</h2>" . "<br>" . "<h4>" . $row['newTime'] . "</h4>" . "<p>" . $row['content'] . "</p>". "<br>". "</div>" . "<br>" . "<br>" . "<div>" . "<form class='forum' action='comments.php' method='POST'>" ."<div class='form-group'>" . "<input type='text' name='Sender' placeholder='Profil: 1 or up'>". "<br>" . "</div>". "<div class='form-group'>" . "<input class='content' type='text' name='forum' placeholder='Hvilke Forum. 1 og op efter.'>" . "<br>". "</div>" . "<div class='form-group'>" ."<textarea type='text' name='Content' placeholder='Skive hvad du synes.'>" . "</textarea>" . "<br>" . "</div>" . "<button type='submit' name='submit'>Send</button>" . "</form>" . "</div>";
+			while ($row = mysqli_fetch_array($result2)) {
+			echo "<div>" . "<h3>" . 'Comments' . "</h3>" . "<h3>" . $row['fuldeNavn'] . "</h3>" . "<h4>" . $row['newTime'] . "</h4>" . "<p>" . $row['content'] . "</p>". "<form class='forum' action='comments.php' method='POST'>" ."<div class='form-group'>" . "<input type='text' name='Sender' placeholder='Profil: 1 or up'>". "<br>" . "</div>". "<div class='form-group'>" . "<input class='content' type='text' name='forum' placeholder='Hvilke Forum. 1 og op efter.'>" . "<br>". "</div>" . "<div class='form-group'>" ."<textarea type='text' name='Content' placeholder='Skive hvad du synes.'>" . "</textarea>" . "<br>" . "</div>" . "<button type='submit' name='submit'>Send</button>" . "</form>" . "</div>";
+}}
+
+	
 	}
 /*		$sql = "SELECT bruger.fuldNavn, comment.newTime, comment.content FROM comment INNER JOIN bruger ON comment.profilId=bruger.id ORDER BY newTime DESC;";
 	$result = mysqli_query($conn, $sql);
